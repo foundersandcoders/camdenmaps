82c2511a5fc71bea78aaa19dbc1dcfab22dfc2b7
William Fisher <williamfisher@hotmail.co.uk>
2014-12-08 22:55:46 +0000

    adds file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
-var gulp = require("gulp");
+//load gulp and plugins
+var gulp = require("gulp"),
+    eslint = require("gulp-eslint"),
+    watch = require("gulp-watch");
 
 //arrays of files to watch for testing and linting
 var serverFiles = [];
 var angularFiles = [];
 var allFiles = serverFiles.concat(angularFiles);
 
-gulp.task("default", function () {
+//task for linting
+gulp.task("lint", function () {
+    return gulp.src(["js/**/*.js"])
+         .pipe(eslint())
+         .pipe(eslint.format())
+         .pipe(eslint.failOnError());
+});
+
+//only run if lint passes
+gulp.task("default", ["lint"], function () {
   
   //task for test
   //test: lab for server, protractor for e2e angular, karma for unit angular
-
-  //task for linting
-  //lint: gulp-jshint
 
   //task for watching
   //watch: lint, test, uglify, build
