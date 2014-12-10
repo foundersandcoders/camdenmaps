a3ba602dbf9264a3e816b2d59e002c84b7f7151a
William Fisher <williamfisher@hotmail.co.uk>
2014-12-10 19:34:42 +0000

    adds .editorconfig

---
+++ test/frontend/unit/test.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict";    
+
+    describe("", function () {
+
+        beforeEach("", function () {
+            var app = angular.module("testapp");
+
+            inject(function($dependencies){
+                
+            });
+        });
+
+        it("", function () {
+
+        });
+
+    });
+
+}());
