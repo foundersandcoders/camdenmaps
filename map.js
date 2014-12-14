f83042c48eb17f133d6cec8ae8ab96e5e4e750c0
Emma M <ekmilner@gmail.com>
2014-12-14 19:00:54 +0000

    changes map source to OSM

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ var map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.OSM
+            source: new ol.source.OSM()
           })
         ],
         view: new ol.View({
