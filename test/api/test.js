e7bad23a8d6b5a5399af557a089921bcfa74a251
NataliaLKB <natalialeebaltazar@gmail.com>
2014-12-14 20:05:15 +0000

    two beginning tests

--- test/api/test.js
+++ test/api/test.js
@@ -6,47 +6,61 @@
         Fs = require('fs'),
         faketoe = require("faketoe"),
         server = require("../../server/server.js"),
+        fixtures = require("../fixtures/xml-test.js"),
+        xml = fixtures.xml,
+        json = fixtures.json,
         describe = lab.experiment,
         it = lab.test,
-        expect = require("code").expect;
+        expect = require("code").expect,
+        options = {
+            method: "GET",
+            url: "/services/police%20station/locations/nw1%200jh"
+        };
+
 
     describe("As a server, I want to call Where's my Nearest current API", function () {
 
-        describe("Given an xml sample", function () {
-            it("then faketoe converts to JSON", function (done) {
+        describe("Given that server starts", function () {
 
-                var parser = faketoe.createParser(function (error, result) {
-                    if (error) return error;
-                    
-                    return result;
-                });
-                
-                var got =  Fs.createReadStream('./fixture-test.xml').pipe(parser);
-
-                expect(typeof(got)).to.equal("object")
-
+            it("then runs on port 8080", function (done) {
+                expect(server.info.port).to.equal(8080);
                 done();
             });
 
-    //     describe("Given that...", function () {
+        });
 
-    //         it("then...", function () {
+        describe("Given that server starts", function () {
 
-    //         });
+            it("statusCode is 200 okay", function (done) {
 
-    //     });
+                server.inject(options, function(response) {
+                    expect(response.statusCode).to.equal(200)
+                    done();
+                });
+
+            });
+
+        });
+
+        describe("Given that the api returns an xml sample", function () {
+            it("then faketoe converts it to a JSON object", function (done) {
+
+                // var parser = faketoe.createParser(function (error, result) {
+                //     if (error) return error;
+
+                //     return result;
+                // });
+                
+                // var got =  Fs.createReadStream(xml).pipe(parser);
+
+                    server.inject(options, function(response) {
+                        console.log(response);
+                        console.log(response.result);
+                    })
+            });
+
+        
 
         })
-    
-    // describe("As a server, I want to ", function () {
-
-    //     describe("Given that...", function () {
-
-    //         it("then...", function () {
-
-    //         });
-
-    //     });
-
     })
 }());
