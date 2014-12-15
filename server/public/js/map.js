a00411b93d8a89796081d8a84956600bc9827876
Emma M <ekmilner@gmail.com>
2014-12-15 11:36:29 +0000

    changes routes to new file location

---
+++ server/public/js/map.js
@@ -0,0 +1,13 @@
+// loads the OSM map centred on Camden Town
+var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.transform([-0.139991, 51.535923], "EPSG:4326", "EPSG:3857"),
+          zoom: 14
+        })
+      });
