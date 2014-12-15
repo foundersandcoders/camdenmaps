4ceab42cd7200d3f2e7ac12d1157d3a17edb6b6d
Emma M <ekmilner@gmail.com>
2014-12-15 10:39:42 +0000

    adds file changes which were being ignored before

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+// loads the OSM map centred on Camden Town
 var map = new ol.Map({
         target: 'map',
         layers: [
@@ -10,3 +11,27 @@ var map = new ol.Map({
           zoom: 14
         })
       });
+
+
+
+// search button functionality
+function fetchJSONFile(path, callback) {
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = function() {
+          if (httpRequest.readyState === 4) {
+              if (httpRequest.status === 200) {
+                  var data = JSON.parse(httpRequest.responseText);
+                  if (callback) callback(data);
+              }
+          }
+      };
+      httpRequest.open('GET', "http://maps.camden.gov.uk/nearest/nearestrest.aspx/services/");
+      httpRequest.send();
+  }
+
+
+  fetchJSONFile("http://maps.camden.gov.uk/nearest/nearestrest.aspx/services/" + "library", function(data){
+      // do something with your data
+      console.log(data);
+  });
+
