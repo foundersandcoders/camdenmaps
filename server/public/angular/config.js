e524cf9dc99f11e9e26ba4038e4d0f834fbcb8ca
William Fisher <williamfisher@hotmail.co.uk>
2014-12-27 14:37:51 +0000

    adds angular router layout

--- server/public/angular/config.js
+++ server/public/angular/config.js
@@ -4,7 +4,49 @@
 */
 
 module.exports = [
+"$urlRouterProvider",
+"$stateProvider",
+function ($urlRouterProvider, $stateProvider) {
 
-function () {
+    //default state is /landing
+    $urlRouterProvider.otherwise("/landing");
+
+    $stateProvider
+        .state("root", {
+            abstract: true,
+            url:"/",
+            templateUrl: "partials/root.html"
+            controller: "RootController"
+        })
+        .state("root.landing", {
+            url: "",
+            templateUrl: "partials/root.landing.html",
+            controller: "LandingController" 
+        })
+        .state("root.landing.services", {
+            url: "/services",
+            templateUrl: "partials/root.landing.services.html"
+            controller: "ServicesController" 
+        })
+        .state("root.address-search", {
+            url: "/:service/search",
+            templateUrl: "partials/root.address-search.html",
+            controller: "SearchController"
+        })
+        .state("root.address-found", {
+            url: "/:service/location/:address",
+            templateUrl: "partials/root.address-found.html",
+            controller: "LocationController" 
+        })
+        .state("root.address-found.list-view", {
+            url: "/:service/location/:address/list",
+            templateUrl: "partials/root.address-found.list-view.html",
+            controller: "ViewListController" 
+        })
+        .state("root.address-found.single-view", {
+            url: "/:service/location/:address/:id",
+            templateUrl: "partials/root.address-found.single-view.html" 
+            controller: "ViewSingleController" 
+        })
 
 }];
---
+++ server/public/angular/controllers/landing-controller.js
@@ -0,0 +1,39 @@
+/****************************************
+*   LANDING-CONTROLLER.JS
+*
+*
+*****************************************/
+;(function () {
+    "use strict";
+
+    module.exports = [
+        "$location",
+        "$scope",
+        function ($location) {
+
+            var button, 
+                //stores function names and corresponding paths for landing-page buttons
+                buttons {
+                //for Find Your Nearest button
+                getNearest: "/services",
+                //for Live Streetworks button
+                getStreetworks: "/streetworks/search",
+                //for About Your Neighbourhood button
+                getNeighbourhood: "/neighbourhood/search"
+            };
+
+            //creates event handler that redirects client to newPath
+            function makeRedirectHandler (newPath) {
+                return function redirectHandler() {
+                    $location.path(newPath);
+                }
+            }
+
+            //create redirectHandler for each button
+            for (button in buttons) {
+                $scope[button] = makeRedirectHandler(buttons[button])
+            }
+        }
+    ];
+
+}());
