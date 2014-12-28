4dd6abd597477280a1c32e8b4e52f3830b2706aa
FilWisher <williamfisher@hotmail.co.uk>
2014-12-28 19:08:18 +0000

    adds services controller

--- server/public/angular/config.js
+++ server/public/angular/config.js
@@ -1,52 +1,64 @@
-/*  CONFIG.JS
+/******************************************************************** 
+*   CONFIG.JS
 *   Description: Defines configuration and routes for angular app.
 *   Use: Registered with core angular module in app.js
-*/
+*
+*********************************************************************/
 
-module.exports = [
-"$urlRouterProvider",
-"$stateProvider",
-function ($urlRouterProvider, $stateProvider) {
+;(function() {
+    "use strict";
 
-    //default state is /landing
-    $urlRouterProvider.otherwise("/landing");
+    module.exports = [
+    "$urlRouterProvider",
+    "$stateProvider",
+    function ($urlRouterProvider, $stateProvider) {
 
-    $stateProvider
-        .state("root", {
-            abstract: true,
-            url:"/",
-            templateUrl: "partials/root.html"
-            controller: "RootController"
-        })
-        .state("root.landing", {
-            url: "",
-            templateUrl: "partials/root.landing.html",
-            controller: "LandingController" 
-        })
-        .state("root.landing.services", {
-            url: "/services",
-            templateUrl: "partials/root.landing.services.html"
-            controller: "ServicesController" 
-        })
-        .state("root.address-search", {
-            url: "/:service/search",
-            templateUrl: "partials/root.address-search.html",
-            controller: "SearchController"
-        })
-        .state("root.address-found", {
-            url: "/:service/location/:address",
-            templateUrl: "partials/root.address-found.html",
-            controller: "LocationController" 
-        })
-        .state("root.address-found.list-view", {
-            url: "/:service/location/:address/list",
-            templateUrl: "partials/root.address-found.list-view.html",
-            controller: "ViewListController" 
-        })
-        .state("root.address-found.single-view", {
-            url: "/:service/location/:address/:id",
-            templateUrl: "partials/root.address-found.single-view.html" 
-            controller: "ViewSingleController" 
-        })
+        //default state is /landing
+        $urlRouterProvider.otherwise("/landing");
 
-}];
+        $stateProvider
+            .state("root", {
+                abstract: true,
+                url:"/",
+                templateUrl: "partials/root.html"
+                controller: "RootController"
+            })
+
+            .state("root.landing", {
+                url: "",
+                templateUrl: "partials/root.landing.html",
+                controller: "LandingController" 
+            })
+  
+            .state("root.landing.services", {
+                url: "/services",
+                templateUrl: "partials/root.landing.services.html"
+                controller: "ServicesController" 
+            })
+  
+            .state("root.address-search", {
+                url: "/:service/search",
+                templateUrl: "partials/root.address-search.html",
+                controller: "SearchController"
+            })
+  
+            .state("root.address-found", {
+                url: "/:service/location/:address",
+                templateUrl: "partials/root.address-found.html",
+                controller: "LocationController" 
+            })
+  
+            .state("root.address-found.list-view", {
+                url: "/:service/location/:address/list",
+                templateUrl: "partials/root.address-found.list-view.html",
+                controller: "ViewListController" 
+            })
+  
+            .state("root.address-found.single-view", {
+                url: "/:service/location/:address/:id",
+                templateUrl: "partials/root.address-found.single-view.html" 
+                controller: "ViewSingleController" 
+            })
+
+    }];
+}());
--- server/public/angular/controllers/services-controller.js
+++ server/public/angular/controllers/services-controller.js
@@ -4,83 +4,106 @@
 *
 ********************************************/
 
+//TODO: Write menu JSON
+//TODO: Write service to access menu
+//TODO: Preload visible items in a resolve
+
 ;(function () {
     "use strict";
 
     module.exports = [
+        "$scope",
+        "$location",
+        "menu",
+        function ($scope, $location, menu) {
+       
+            /***************** MENU STATE VARIABLES ****************/
 
-        function () {
-            
-            //stores current menu items
-            var visualMenu = [],
-                //stores menu and menu functions
-                menuControl = (function createReloader () {
-                //currentMenu position accessible only to menuControl
-                var menuSize = 4;
-                    currentMenu = "root";
-                return {
-                    //function loads first 4 items of current menu
-                    reloadMenu: function reloadMenu () {
-                        var i;
-                        for (i = 0; i < menuSize; i += 1) {
-                            visualMenu[i] = currentMenu[i];
-                        } 
-                    },
-                    changeCurrent: function changeCurrent (newMenu) {
-                        currentMenu += "." + newMenu + ".content";
-                    },
-                    backMenu: function backMenu () {
-                        if (currentMenu === "root") {
-                            return;
-                        } else {
-                            currentMenu = currentMenu
-                                            .split(".")
-                                            .slice(0, currentMenu.split(".").length-2)
-                                            .join(".");
-                        }
+            //exposes current menu items to be displayed
+            $scope.visibleItems = [];
+            //current position in menu
+            var currentPosition = 0, 
+                currentCategory = getCurrentItems,
+                currentIndex = 0,
+                menuSize = 4;
+
+            /***************** MENU POPULATION FUNCTIONS ****************/
+
+            //loads current items based on current position
+            function getCurrentItems (done) {
+                currentCategory = menu.filter(function menuFilter (item) {
+                    return item.parentId === currentPosition;
+                });
+                done(currentIndex);
+            }
+
+            //loads visible items
+            function loadVisibleItems (start) {
+                //loads first 4 items from current category into visible
+                $scope.visibleItems = currentCategory.slice(start, start+menuSize);
+                //adds click handlers to each visible item
+                (function addClickHandlers (index) {
+                    var item = $scope.visibleItems[index];
+                    if (index > $scope.visibleItems.length) {
+                        return;
                     }
-                }
-            }());
+                    item.handler = returnItemClickHandler(item);
+                    addClickHandlers(index+1);
+                }(0));
+            }
 
-            //TODO: service/category data structure
+            /***************** MENU CONTROL FUNCTIONS ****************/
 
-            //TODO: currently displaying service/category
-           
-            //TODO: left and right button handlers
-
-            //TODO: service/category button handlers
-            function addHandler (item) {
+            //returns click handler for menu items
+            function returnItemClickHandler (item) {
                 return function () {
+                    var destination;
                     if (item.type === "service") {
-                        var path = "/" + item.name + "/search"
-                        $location.path(path);
+                        destination = "/" + item.text + "/search";
+                        $location.path(destination);
                     } else if (item.type === "category") {
-                        //TODO: Change current directory
-                        menuControl.changeCurrent(item.name);
-                        //reload items
-                        menuControl.reloadMenu();
+                        currentIndex = 0;
+                        currentPosition = item.id;
+                        getCurrentItems(loadVisibleItems);
                     }
                 }
             }
+             
+            //loads next items in menu   
+            function nextItems () {
+                if (currentIndex + menuSize > currentCategory.length) {
+                    return;
+                } else {
+                    currentIndex += menuSize;
+                    loadVisibleItems(currentIndex);
+                }
+            }
 
+            //returns to previous category
+            function backCategory () {
+                currentPosition = currentCategory[0].parentId;
+                getCurrentItems(loadVisibleItems);
+            }
+             
+            //loads previous items in menu   
+            function prevItems () {
+                if (currentIndex - menuSize < 0) {
+                    currentIndex = 0;
+                    loadVisibleItems(currentIndex);
+                } else {
+                    currentIndex -= menuSize;
+                    loadVisibleItems(currentIndex);
+                }
+            }
 
+            //bind to next button
+            $scope.next = nextItems;
+            //bind to prev button
+            $scope.prev = prevItems;
+            //bind to back button
+            $scope.back = backCategory;
 
         }
     ];
-
-    function intoCategory (newCurrent) {
-        currentMenu = newCurrent;
-    }
-
-    function loadVisualMenu () {
-        //return array of items in current menu
-        var i,
-            currentMenuItems = fullMenu.filter(function (item) {
-                return item.parentId === currentMenu;
-            });
-        for ( i = 0; i < menuSize; i += 1 ) {
-            visualMenu[i] = currentMenuItems[i];
-        } 
-    }
 
 }());
