2505ca7d91c0bfc2b185f7a124bed1e7bc8d1e5e
Emma M <ekmilner@gmail.com>
2014-12-31 15:50:09 +0000

    bounds map to roughly Camden council area

--- server/public/angular/app.js
+++ server/public/angular/app.js
@@ -6,4 +6,49 @@
 (function () {
     "use strict";
      var app = angular.module("map", ["leaflet-directive"]);
+
+     	app.controller("SimpleMapController", [ '$scope', function($scope) {
+	    angular.extend($scope, {
+	        defaults: {
+	            scrollWheelZoom: false
+	        }
+	    });
+	}]);
+
+	app.controller("CustomParametersController", [ '$scope', function($scope) {
+	    var regions = {
+        		london: {
+		            northEast: {
+		                lat: 51.57878,
+		                lng: -0.094538
+		            },
+		            southWest: {
+		                lat: 51.510989,
+		                lng: -0.218649
+		            }
+        		}
+	       	};
+
+	    $scope.setRegion = function(region) {
+	        if (!region) {
+	            $scope.maxbounds = {};
+	        } else {
+	            $scope.maxbounds = regions[region];
+	        }
+	    };
+
+	    angular.extend($scope, {
+	        camden: {
+	            lat: 51.535923,
+	            lng: -0.139991,
+	            zoom: 14
+	        },
+	        maxbounds: regions.london,
+        	defaults: {
+            scrollWheelZoom: false
+        	}
+	        
+	    });
+	}]);
+
 }());
--- server/public/angular/controllers/controllers.js
+++ server/public/angular/controllers/controllers.js
@@ -7,23 +7,6 @@
     "use strict";
 //      var controllerModule = angular.module("ControllerModule", []);
 //      controllerModule.controller("ControllerName", require(importedController));
-	app.controller("SimpleMapController", [ '$scope', function($scope) {
-	    angular.extend($scope, {
-	        defaults: {
-	            scrollWheelZoom: false
-	        }
-	    });
-	}]);
 
-	app.controller("CustomParametersController", [ '$scope', function($scope) {
-	    angular.extend($scope, {
-	        camden: {
-	            lat: 51.535923,
-	            lng: -0.139991,
-	            zoom: 14
-	        }
-	       
-	    });
-	}]);
 
 }());
