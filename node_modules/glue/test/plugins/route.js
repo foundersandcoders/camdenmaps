44c45468d46d43a5e3733fa7a71b673a94209550
NataliaLKB <natalialeebaltazar@gmail.com>
2015-01-12 17:06:19 +0000

    gitignore

---
+++ node_modules/glue/test/plugins/route.js
@@ -0,0 +1,11 @@
+exports.register = function (server, options, next) {
+    server.route({method: 'GET', path: 'plugin', handler: function (request, reply) {
+        reply();
+    }});
+    next();
+};
+
+exports.register.attributes = {
+    name: 'route',
+    multiple: true
+};
