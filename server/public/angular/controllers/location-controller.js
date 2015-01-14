73f7899ec7407da163ed1f3b64653698ebeff9e7
Emma M <ekmilner@gmail.com>
2015-01-14 17:18:25 +0000

    changes mock data to acutal data

--- server/public/angular/controllers/location-controller.js
+++ server/public/angular/controllers/location-controller.js
@@ -21,12 +21,11 @@
                 .success(function success (data) {
                     $scope.updateResults(data.properties);
                     $scope.updateLocation(data.location);
+                    $scope.addMarkers();
                 });
 
             $scope.service = $stateParams.service;
             $scope.address = $stateParams.address.toUpperCase();
-
-            console.log('item', $scope.item);
 
             $scope.searchAgain = function searchAgain () {
                 //TODO: write logic for function
@@ -48,7 +47,7 @@
             };
 
             $scope.toggle = function toggle() {
-                if(!called) { called = true; console.log('list results called'); return $scope.listResults(); }
+                if(!called) { called = true; return $scope.listResults(); }
                 $scope.exit();
                 called = false;
             };
@@ -64,209 +63,18 @@
 
             $scope.addMarkers = function addMarkers() {
 
-            //until XML is correctly parsed, using fake data that mocks the correct JSON format we want
-            var fakeData = {
-              "Locations": {
-                "-Area": "WC1H 9JE",
-                "AddressSearchResults": {
-                  "-Area": "WC1H 9JE",
-                  "-East": "182795",
-                  "-North": "530135",
-                  "-Longitude": "-0.12536",
-                  "-Latitude": "51.52908"
-                },
-                "Properties": {
-                  "Property": [
-                    {
-                      "-LocationID": "530521182287",
-                      "-BuildingName": "Corams Fields ",
-                      "-StreetNum": "93",
-                      "-Street": "Guilford Street",
-                      "-PostCode": "WC1N 1DN",
-                      "-Longitude": "-0.11999",
-                      "-Latitude": "51.52442",
-                      "-ViewLat": "51.52356",
-                      "-ViewLng": "-0.11946",
-                      "-View": "338",
-                      "-Distance": "0.638",
-                      "PoI": {
-                        "-Name": "Brownies Holborn 3Rd",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    },
-                    {
-                      "-LocationID": "530533182040",
-                      "-BuildingName": "Great Ormond Street Childrens Hospital ",
-                      "-Street": "Great Ormond Street",
-                      "-PostCode": "WC1N 3JH",
-                      "-Longitude": "-0.11991",
-                      "-Latitude": "51.5222",
-                      "-ViewLat": "51.52204",
-                      "-ViewLng": "-0.11976",
-                      "-View": "331",
-                      "-Distance": "0.853",
-                      "PoI": {
-                        "-Name": "Guides Holborn 17Th",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    },
-                    {
-                      "-LocationID": "530356181415",
-                      "-BuildingName": "Dragon Hall ",
-                      "-StreetNum": "17",
-                      "-Street": "Stukeley Street",
-                      "-PostCode": "WC2B 5LL",
-                      "-Longitude": "-0.12269",
-                      "-Latitude": "51.51663",
-                      "-ViewLat": "51.51657",
-                      "-ViewLng": "-0.12259",
-                      "-View": "315",
-                      "-Distance": "1.398",
-                      "PoI": {
-                        "-Name": "Brownies Covent Garden 1St",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    },
-                    {
-                      "-LocationID": "528859184650",
-                      "-BuildingName": "Kentish Town Congregational Church ",
-                      "-Street": "Kelly Street",
-                      "-PostCode": "NW1 8PH",
-                      "-Longitude": "-0.14307",
-                      "-Latitude": "51.54604",
-                      "-ViewLat": "51.54589",
-                      "-ViewLng": "-0.14357",
-                      "-View": "62",
-                      "-Distance": "2.251",
-                      "PoI": {
-                        "-Name": "Brownies St Pancras 8Th",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    },
-                    {
-                      "-LocationID": "527885185667",
-                      "-StreetNum": "53",
-                      "-Street": "Courthope Road",
-                      "-PostCode": "NW3 2LE",
-                      "-Longitude": "-0.15674",
-                      "-Latitude": "51.5554",
-                      "-ViewLat": "51.55547",
-                      "-ViewLng": "-0.15646",
-                      "-View": "248",
-                      "-Distance": "3.648",
-                      "PoI": [
-                        {
-                          "-Name": "Brownies St Pancras 7Th",
-                          "-Type": "Brownies group",
-                          "-Telephone": "0800 169 5901",
-                          "-URL": "http://www.girlguiding.org.uk/interested",
-                          "-Source": "CINDEX"
-                        },
-                        {
-                          "-Name": "Rainbows St Pancras 7Th",
-                          "-Type": "Brownies group",
-                          "-Telephone": "0800 169 5901",
-                          "-URL": "http://www.girlguiding.org.uk/interested",
-                          "-Source": "CINDEX"
-                        }
-                      ]
-                    },
-                    {
-                      "-LocationID": "527001184736",
-                      "-StreetNum": "52",
-                      "-Street": "Belsize Square",
-                      "-PostCode": "NW3 4HN",
-                      "-Longitude": "-0.16982",
-                      "-Latitude": "51.54723",
-                      "-ViewLat": "51.54735",
-                      "-ViewLng": "-0.16974",
-                      "-View": "201",
-                      "-Distance": "3.686",
-                      "PoI": {
-                        "-Name": "Brownies Hampstead 17Th",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    },
-                    {
-                      "-LocationID": "526942185341",
-                      "-BuildingName": "Olave Centre ",
-                      "-StreetNum": "12C",
-                      "-Street": "Lyndhurst Road",
-                      "-PostCode": "NW3 5PQ",
-                      "-Longitude": "-0.17045",
-                      "-Latitude": "51.55268",
-                      "-ViewLat": "51.55312",
-                      "-ViewLng": "-0.17064",
-                      "-View": "164",
-                      "-Distance": "4.084",
-                      "PoI": [
-                        {
-                          "-Name": "Brownies Hampstead 12Th",
-                          "-Type": "Brownies group",
-                          "-Telephone": "0800 169 5901",
-                          "-URL": "http://www.girlguiding.org.uk/interested",
-                          "-Source": "CINDEX"
-                        },
-                        {
-                          "-Name": "Rainbows Hampstead Pax Lodge",
-                          "-Type": "Brownies group",
-                          "-Telephone": "07787 551771",
-                          "-URL": "http://www.girlguiding.org.uk/interested",
-                          "-Source": "CINDEX"
-                        }
-                      ]
-                    },
-                    {
-                      "-LocationID": "525297185872",
-                      "-BuildingName": "St Lukes Church Hall ",
-                      "-Street": "Kidderpore Avenue",
-                      "-PostCode": "NW3 7SU",
-                      "-Longitude": "-0.19398",
-                      "-Latitude": "51.55782",
-                      "-ViewLat": "51.55768",
-                      "-ViewLng": "-0.19413",
-                      "-View": "31",
-                      "-Distance": "5.734",
-                      "PoI": {
-                        "-Name": "Brownies West Hampstead 2Nd",
-                        "-Type": "Brownies group",
-                        "-Telephone": "0800 169 5901",
-                        "-URL": "http://www.girlguiding.org.uk/interested",
-                        "-Source": "CINDEX"
-                      }
-                    }
-                  ]
-                }
-              }
-            };
                     //once we have real data this will need to be changed to data not fakeData
-                    var root = fakeData.Locations.Properties;
+                    var root = $scope.results;
 
-                    console.log(root.Property[1]["-StreetNum"] !== undefined);
-
+                    console.log('results', $scope.results);
 
 
                     function lat(i){
-                        return Number(root.Property[i]["-Latitude"]);
+                        return Number($scope.results[i]["Latitude"]);
                     }
 
                     function lng(i) {
-                        return Number(root.Property[i]["-Longitude"]);
+                        return Number(root[i]["Longitude"]);
                     }
 
                     function message(i) {
@@ -280,12 +88,12 @@
                         var pointMessage = "";
 
                         //TODO work out a sensible way to loop over this.... 
-                        check(root.Property[i]["PoI"]["-Name"] || root.Property[i]["PoI"][0]["-Name"], "<br>"); 
-                        check(root.Property[i]["-BuildingName"], "<br>");
-                        check(root.Property[i]["-StreetNum"], " ");
-                        check(root.Property[i]["-Street"], "<br>"); 
-                        check(root.Property[i]["-PostCode"], "<br>");
-                        check(root.Property[i]["PoI"]["-Telephone"], "");
+                        check(root[i]["display"]["Name"] || root[i]["display"][0]["-Name"], "<br>"); 
+                        check(root[i]["BuildingName"], "<br>");
+                        check(root[i]["View"], " ");
+                        check(root[i]["Street"], "<br>"); 
+                        check(root[i]["PostCode"], "<br>");
+                        check(root[i]["display"]["Telephone"], "");
 
                         // return PoIName + "<br>" + root.Property[i]["-BuildingName"] + "<br>" + root.Property[i]["-StreetNum"] + " " + root.Property[i]["-Street"] + "<br>" + root.Property[i]["-PostCode"] + "<br>" + root.Property[i]["PoI"]["-Telephone"];
 
@@ -341,10 +149,15 @@
                     };
 
                 $scope.updateMarkers(firstEight);
+                console.log('results2', $scope.results);
 
                 };
 
                 $scope.addMarkers();
+                console.log('results3', $scope.results);
+
+
+
 
         }
     ];
