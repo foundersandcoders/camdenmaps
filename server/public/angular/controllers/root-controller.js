8648e90982cc448ebafb06ae862ade67e36c6175
Emma M <ekmilner@gmail.com>
2015-01-15 17:34:08 +0000

    moves addmarker function constructor to root controller

--- server/public/angular/controllers/root-controller.js
+++ server/public/angular/controllers/root-controller.js
@@ -22,6 +22,8 @@
                 $scope.locationSelected = newLocation;
             };
 
+            //************ MAP MANIPULATIONS ***************
+
             var regions = {
                 camdenBorough: {
                     northEast: {
@@ -64,6 +66,93 @@
                 $scope.centre = newCentre;
             };
             
+            $scope.addMarkers = function addMarkers() {
+
+                    var root = $scope.results;
+
+                    var lat = function lat(i){
+                        return Number($scope.results[i]["Latitude"]);
+                    }
+
+                    var lng = function lng(i) {
+                        return Number(root[i]["Longitude"]);
+                    }
+
+                    var message = function message(i) {
+                        //this stops undefined being returned in message
+                        var check = function(value, spacing) {
+                            if(value !== undefined) {
+                                pointMessage += (value + spacing);
+                            }
+                        };
+
+                        var pointMessage = "";
+
+                        //TODO work out a sensible way to loop over this.... 
+                        //TODO replace view with nothing if it returns 0, ref car parks
+                        check(root[i]["display"]["Name"] || root[i]["display"][0]["Name"], "<br>"); 
+                        check(root[i]["BuildingName"], "<br>");
+                        check(root[i]["StreetNum"], " ");
+                        check(root[i]["Street"], "<br>"); 
+                        check(root[i]["PostCode"], "<br>");
+                        check(root[i]["display"]["Telephone"], "");
+
+                        // return PoIName + "<br>" + root.Property[i]["-BuildingName"] + "<br>" + root.Property[i]["-StreetNum"] + " " + root.Property[i]["-Street"] + "<br>" + root.Property[i]["-PostCode"] + "<br>" + root.Property[i]["PoI"]["-Telephone"];
+
+                        return pointMessage;
+                    };
+
+                    //this hard coding is for development purposes - *MUST* be changed
+                    //only returns the first Name when there are more than one
+                    var firstEight = {
+
+                        m1: {
+                            lat: lat(0),
+                            lng: lng(0),
+                            message: message(0)
+
+                        },
+                        m2: {
+                            lat: lat(1),
+                            lng: lng(1),
+                            message: message(1)
+                        },
+                        m3: {
+                            lat: lat(2),
+                            lng: lng(2),
+                            message: message(2)
+                        },
+                        m4: {
+                            lat: lat(3),
+                            lng: lng(3),
+                            message: message(3)
+                        },
+                        m5: {
+                            lat: lat(4),
+                            lng: lng(4),
+                            message: message(4)
+                        },
+                        m6: {
+                            lat: lat(5),
+                            lng: lng(5),
+                            message: message(5)
+                        },
+                        m7: {
+                            lat: lat(6),
+                            lng: lng(6),
+                            message: message(6)
+                        },
+                        m8: {
+                            lat: lat(7),
+                            lng: lng(7),
+                            message: message(7)
+                        }
+
+                    };
+
+                    $scope.updateMarkers(firstEight);
+
+                };
 
         }  
     ];
