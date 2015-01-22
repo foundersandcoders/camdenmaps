94d2f1b2bc5e6a038017add5fe6457a2691a079f
William Fisher <williamfisher@hotmail.co.uk>
2015-01-22 16:01:26 +0000

    adds todos and validation reminders

--- server/handlers/convertXml.js
+++ server/handlers/convertXml.js
@@ -1,3 +1,5 @@
+//TODO: validate that response has x,y or z properties and if not, respond nothing 
+
 ;(function() { 
     "use strict";
 
@@ -8,38 +10,38 @@
     var serviceArray = Config.map.serviceArrays;
 
     module.exports = {
-        //Function for responding JSON to client
-        //Function for responding JSON to client
+        
+        //function for responding JSON to client
         convertToJson: function convertToJson (err, res, req, rep) {
           
             if (serviceArray.parking.indexOf(req.params.service) === -1 &&
                 serviceArray.recycling.indexOf(req.params.service) === -1) {
                 
-                    // var parser = xml.parse(res);
+                // var parser = xml.parse(res);
                 var xml = '';
                 var response = {};
                 response.properties = [];
 
                 res.on('data', function(data){
-                  xml = xml + data;
-                }).on('end', function(){
-                  parser.parseString(xml, function (err, result) {
-                    // console.log(result);
-                    response.location = result.Locations.AddressSearchResults[0]['$'];
-                    result.Locations.Properties[0].Property.map(function(p) {
-                      var formatProperty = p['$'];
-                      formatProperty.display = p.PoI[0]['$']
-                      response.properties.push(formatProperty);
-                    });
-
-                    rep(response);
-                  });
+                    xml = xml + data;
                 });
+
+                res.on('end', function(){
+                    parser.parseString(xml, function (err, result) {
+                        response.location = result.Locations.AddressSearchResults[0]['$'];
+                        result.Locations.Properties[0].Property.map(function(p) {
+                            var formatProperty = p['$'];
+                            formatProperty.display = p.PoI[0]['$']
+                            response.properties.push(formatProperty);
+                        });
+                        rep(response);
+                    });
+                });
+            
             } else {
+               //TODO: construct response differently if parking or recycling
                 rep(res);
             }
-            // });
         }
-        
     };
 }());
