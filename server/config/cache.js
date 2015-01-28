43accabd85a602721c785d8bd7299bda1cec8801
Natalia Baltazar <natalialeebaltazar@gmail.com>
2015-01-28 20:46:07 +0000

    Update cache.js

--- server/config/cache.js
+++ server/config/cache.js
@@ -5,6 +5,8 @@
 ************************************************/
 
 var Nodecache = require("node-cache");
-var cache = new Nodecache({ stdTTL: 10000000000000000 });
+
+// 86400000 is for 24 hours
+var cache = new Nodecache({ stdTTL: 86400000 });
 
 module.exports = cache;
