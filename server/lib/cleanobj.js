743c219ec969dd221e6916c542406a90e42081ef
NataliaLKB <natalialeebaltazar@gmail.com>
2015-02-04 18:19:31 +0000

    ensuring that `/` is replaced with `and` in find your nearest endpoints

--- server/lib/cleanobj.js
+++ server/lib/cleanobj.js
@@ -8,6 +8,7 @@
         for(prop in obj) {
             if(typeof obj[prop] === "string") {
                 obj[prop] = strip(obj[prop]);
+                obj[prop] = obj[prop].replace("/", " and ");
             }
         }
         return obj;
