3c61b3819564d7072f206d432bc77ecf75c27d4e
NataliaLKB <natalialeebaltazar@gmail.com>
2015-02-06 18:41:38 +0000

    testing endpoints with results equaling 5 or 10

--- test/frontend/acceptance/cachservices.js
+++ test/frontend/acceptance/cachservices.js
@@ -4,36 +4,80 @@
 *   Use: run tests by npm test
 **************************************************/
 
-var menu = require("../../../server/angular/menu.json");
+// var menu = require("../../../server/angular/menu.json");
 
-var errorMessage = '{"statusCode":500,"error":"Internal Server Error","message":"An internal server error occurred"}';
+// var errorMessage = '{"statusCode":500,"error":"Internal Server Error","message":"An internal server error occurred"}';
 
-(function () {
-    "use strict";
+// (function () {
+//     "use strict";
 
-    describe("caching service", function () {
+//For catching 500 errors
+    // describe("caching service", function () {
 
-        beforeEach(function() {
-            return browser.ignoreSynchronization = true;
-        });
+    //     beforeEach(function() {
+    //         return browser.ignoreSynchronization = true;
+    //     });
 
-        function runTest (j) {
-            it("service cached" + menu[j].title, function () {
+    //     function runTest (j) {
+    //         it("service cached" + menu[j].title, function () {
 
-                browser.get("/services/" + menu[j].title );
-    			var text = element(by.tagName("pre")).getText();
+    //             browser.get("/services/" + menu[j].title );
+    // 			var text = element(by.tagName("pre")).getText();
 
-                expect(text).toNotEqual(errorMessage);
-            });
+    //             expect(text).toNotEqual(errorMessage);
+    //         });
 
-        }
+    //     }
 
-        for (var i = 0; i < menu.length ; i++ ) {
-            if (menu[i].type === 'service') {
-                runTest(i);
+    //     for (var i = 0; i < menu.length ; i++ ) {
+    //         if (menu[i].type === 'service') {
+    //             runTest(i);
 
-            }
-        }
-    });
+    //         }
+    //     }
+    // });
 
-}());
+
+//For catching results if 5 or 10.
+//     describe("caching service", function () {
+
+//         beforeEach(function() {
+//             return browser.ignoreSynchronization = true;
+//         });
+
+//         var i;
+
+//         for (i = 0; i < menu.length ; i++ ) {
+//             if (menu[i].type === 'service') {
+//                 runTest(i);
+
+//             }
+//         }
+
+//         function runTest (j) {
+//             it("service cached" + menu[j].title, function () {
+
+//                 browser.get("/services/" + menu[j].title );
+
+//                 var mytext = element(by.tagName("pre")).getText();
+
+//                 mytext.then(function (text) {
+//                     parse(text);
+//                 });
+
+//                 function parse(jsonString) {
+//                     var obj = JSON.parse(jsonString);
+
+//                     last(obj.properties.length)
+//                 }
+
+//                 function last(num) {
+//                     expect(num).toNotEqual(10);
+//                 }
+
+//             });
+
+//         }
+//     });
+
+// }());
