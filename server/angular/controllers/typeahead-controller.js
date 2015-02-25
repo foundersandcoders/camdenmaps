a5a291e513282bbd5ba63d31eef99f018251a745
StErrorHospital <subhanshaffi@googlemail.com>
2015-02-25 16:44:50 +0000

    minimal typeahead styling

--- server/angular/controllers/typeahead-controller.js
+++ server/angular/controllers/typeahead-controller.js
@@ -16,7 +16,7 @@
         "localStorageService",
         "apiSearch",
         "markers",
-        function ($scope, $location, fetchToken, $http, $stateParams, localStorageService, apiSearch, markers) {
+        function ($scope, $location, buttonHandlers, fetchToken, $http, $stateParams, localStorageService, apiSearch, markers) {
 
             var menu = [],
                 uprnArray = [];
@@ -105,17 +105,19 @@
                                     return;
                                 } else {
 
-                                return response.data.map(function (item){
-                                    item.title = item.Unit + " " +
-                                        item.BuildingName + " " +
-                                        item.BuildingNumber + " " +
-                                        item.Street + " " +
-                                        item.Postcode;
+                                    return response.data.map(function (item){
+                                        item.title = item.Unit + " " +
+                                            item.BuildingName + " " +
+                                            item.BuildingNumber + " " +
+                                            item.Street + " " +
+                                            item.Postcode;
 
-                                    uprnArray.push(item);
+                                        uprnArray.push(item);
 
-                                    return item;
-                                });
+                                        return item;
+                                    });
+                                }
+
                             });
                     };
                 });
