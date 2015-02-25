2da370344815c9edd87ffbe708e34dfa19ec08c7
FilWisher <williamfisher@hotmail.co.uk>
2015-02-25 16:05:55 +0000

    add unit test for /lib/capitalize.js

--- server/lib/capitalize.js
+++ server/lib/capitalize.js
@@ -3,7 +3,7 @@
 
     //capitalize first letter of word (norm
     module.exports = function cap(word) {
-        if(typeof word !== "undefined") {
+        if(typeof word === "string") {
             return word[0].toUpperCase() + word.substring(1, word.length).toLowerCase();
         } else {
             return word;
