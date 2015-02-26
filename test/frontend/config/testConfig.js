f02660679f69388e021d4b098ec80d5109f4456f
NataliaLKB <natalialeebaltazar@gmail.com>
2015-02-26 17:04:04 +0000

    added landing e2e tests

--- test/frontend/config/testConfig.js
+++ test/frontend/config/testConfig.js
@@ -7,7 +7,16 @@
 ;(function() {
     "use strict";
     module.exports = {
-    	
+    	path: {
+    		main: 'http://localhost:8080/',
+    		home: '#/home'
+    	},
+    	landing: {
+    		buttons: {
+    			title: ["Camden Services", "About Your Neighbourhood", "Live Streetworks"],
+    			imgSrc: ["img/icons/find-your-nearest.png", "img/icons/your-neighbourhood.png", "img/icons/streetworks.png"]
+    		}
+    	}
     }
 
 }());
