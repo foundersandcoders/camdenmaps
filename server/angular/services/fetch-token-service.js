b46f8c0d5a3a5b74cfcd4bb18066eecc9794cc26
FilWisher <williamfisher@hotmail.co.uk>
2015-02-26 16:42:39 +0000

    update fetchtoken service

--- server/angular/services/fetch-token-service.js
+++ server/angular/services/fetch-token-service.js
@@ -10,9 +10,20 @@
         "$http",
         function ($http) {
 
+            this.tokenIssued = false;
+
             this.getToken = function getToken () {
-                
-                return $http({method:"GET", url:"http://camdenmaps.herokuapp.com/auth_token"});
+                if(!this.tokenIssued) {
+                    this.tokenIssued = true;
+                    return $http({method:"GET", url:"http://camdenmaps.herokuapp.com/auth_token"});
+                } else {
+               
+                    return {
+                        success: function(cb) {
+                            cb();
+                        }
+                    }
+                }
                 
             }
         }
