1c1da84717264d3ca4d35418069017a98ebc44bc
NataliaLKB <natalialeebaltazar@gmail.com>
2015-03-04 16:23:52 +0000

    added neighbourhood tests

--- gulpfile.js
+++ gulpfile.js
@@ -26,14 +26,15 @@
         karmaTestFiles = ["./test/frontend/unit/*.js"],
         protractorTestFiles = [
                         // Desktop Tests below
-                            './test/frontend/acceptance/desktop/landing.e2e.js',
-                            './test/frontend/acceptance/desktop/services/categories.e2e.js',
-                            './test/frontend/acceptance/desktop/services/services.e2e.js',
-                            './test/frontend/acceptance/desktop/streetworks/streetworks.e2e.js',
+                            // './test/frontend/acceptance/desktop/landing.e2e.js',
+                            // './test/frontend/acceptance/desktop/services/categories.e2e.js',
+                            // './test/frontend/acceptance/desktop/services/services.e2e.js',
+                            // './test/frontend/acceptance/desktop/streetworks/streetworks.e2e.js',
+                            './test/frontend/acceptance/desktop/neighbourhood/neighbourhood.e2e.js',
                         // Mobile Tests below
-                            './test/frontend/acceptance/mobile/landing.e2e.js',
-                            './test/frontend/acceptance/mobile/services/categories.e2e.js',
-                            './test/frontend/acceptance/mobile/services/services.e2e.js',
+                            // './test/frontend/acceptance/mobile/landing.e2e.js',
+                            // './test/frontend/acceptance/mobile/services/categories.e2e.js',
+                            // './test/frontend/acceptance/mobile/services/services.e2e.js',
                             ],
         sassFiles = ["./server/public/css/*.scss", "./server/public/css/*/*.scss"],
         allFiles = serverFiles.concat(angularFiles);
