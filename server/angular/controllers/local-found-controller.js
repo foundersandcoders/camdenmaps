65858cdec83dc0e3e4329e3487b1c24b35835d07
FilWisher <williamfisher@hotmail.co.uk>
2015-03-05 16:40:18 +0000

    add polling station pin on click

--- server/angular/controllers/local-found-controller.js
+++ server/angular/controllers/local-found-controller.js
@@ -21,10 +21,43 @@
                 $location.path("/home/neighbourhood");
             };
 
+            var pollingStationCoordinates;
+
+            $scope.showPollingStation = false;
+
+            function getPollingStationCoordinates (uprn) {
+
+                apiSearch.searchNeighbourhood(uprn)
+                    .success(function(data) {
+
+                        pollingStationCoordinates = {
+
+                            lat: Number(data.location.Latitude),
+                            lng: Number(data.location.Longitude),
+                            //icon: {
+                            //    iconSize: [28]
+                            //}
+
+                        }
+
+                        if($scope.showPollingStation) {
+                            $scope.markers.pollingStation = pollingStationCoordinates;
+                        }
+
+                    });
+
+            }
+
+            function hasPollingStation (data) {
+
+                return data.information.hasOwnProperty("Polling Station")
+
+            }
+
             //search api for uprn
             apiSearch.searchNeighbourhood($stateParams.uprn)
                 .success(function(data) {
-                    
+
                     if (data.hasOwnProperty("error")) {
                         $location.path("/home/neighbourhood");
                         return $scope.updateError(data.message);
@@ -44,7 +77,11 @@
                         lng: (Number(data.location.Longitude) - 0.004),
                         zoom: 15
                     });
-
+                    if (hasPollingStation(data)) {
+                        var pollingStationUPRN = data.information["Polling Station"].Url.split("uprn=").pop();
+                        console.log(pollingStationUPRN);
+                        getPollingStationCoordinates(pollingStationUPRN);
+                    }
                     return $scope.update("information", data.information);
                 })
                 .error(function(data) {
@@ -52,6 +89,17 @@
                     return $location.path("/home/neighbourhood");
                 });
 
+            $scope.togglePollingStation = function() {
+
+                if ($scope.showPollingStation) {
+                    $scope.showPollingStation = false;
+                    delete $scope.markers.pollingStation;
+                } else {
+                    $scope.showPollingStation = true;
+                    $scope.markers.pollingStation = pollingStationCoordinates;
+                }
+
+            }
             //$scope.noLocationNeighbourhood = false;
 
         }
