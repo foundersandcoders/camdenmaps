6b14ef8d4ee28a63d4935d89742e774474a71d3b
William Fisher <williamsykesfisher@gmail.com>
2015-03-09 10:49:48 +0000

    uncomment mobile tests

--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,9 @@
                             './test/frontend/acceptance/desktop/neighbourhood/neighbourhood.e2e.js',
 
                         // Mobile Tests below
-                            // './test/frontend/acceptance/mobile/landing.e2e.js',
-                            // './test/frontend/acceptance/mobile/services/categories.e2e.js',
-                            // './test/frontend/acceptance/mobile/services/services.e2e.js',
+                             './test/frontend/acceptance/mobile/landing.e2e.js',
+                             './test/frontend/acceptance/mobile/services/categories.e2e.js',
+                             './test/frontend/acceptance/mobile/services/services.e2e.js',
                             ],
         sassFiles = ["./server/public/css/*.scss", "./server/public/css/*/*.scss"],
         allFiles = serverFiles.concat(angularFiles);
